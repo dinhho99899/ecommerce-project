ff117354d875db5e431c2ebb98d95e173f152ea7
dinhho99899 <dinhho99899@gmail.com>
2023-04-26 22:04:09 +0700

    a

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -42,7 +42,7 @@ const CheckoutPage = () => {
             <div className='cart-title'>
               <h4>Name</h4>
               <h4>Quantity</h4>
-              <hr />
+              <div></div>
             </div>
             <div className='cart-items'>
               {cart.map((item, index) => {
